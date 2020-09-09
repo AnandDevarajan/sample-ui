25bb8405da44f43814cb91e659e0601cc08b7b5f
Anand Devarajan <ananddevarajan>
2020-09-09 09:45:26 +0530

    signup page

--- src/components/auth/signin.js
+++ src/components/auth/signin.js
@@ -1,15 +1,28 @@
 import React from 'react';
-import {toast,ToastContainer} from 'react-toastify'
 import { Container } from 'reactstrap';
-const signup=()=>{
-    return (
+const signup = () => {
+  return (
+    <div className='row'>
+      <div className='col-sm-3'>
         <Container fluid>
-            <h1 className ="text-center text-dark">
-                Account <span className='text-dark'>SignIn</span>
-            </h1>
-            
+          <h1 className='text-center text-success'>
+            Account <span className='text-dark'>SignIn</span>
+          </h1>
+          <form>
+            <div className='form-group'>
+              <label className='text-dark'>Email</label>
+              <input type='email' name='email' />
+            </div>
+            <div className='form-group'>
+              <label>Password</label>
+              <input type='password' name='password' />
+            </div>
+            <input type='submit' className='btn btn-success btn-block' />
+          </form>
         </Container>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
 export default signup;
